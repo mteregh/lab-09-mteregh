e0b35806af5309cb123230a4b79e865782ca8408
mteregh <mtguerra@miuandes.cl>
2026-05-28 16:23:37 -0400

    Configure Pundit in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
@@ -8,7 +10,30 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # after_action :verify_pundit_authorization, unless: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
+
+  def verify_pundit_authorization
+    if public_page?
+      skip_authorization
+    elsif action_name == "index"
+      verify_policy_scoped
+    else
+      verify_authorized
+    end
+  end
+
+  def public_page?
+    controller_name == "home"
+  end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back_or_to(root_path)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
